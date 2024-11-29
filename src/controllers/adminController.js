31d52f15ba6bd08ca889c5decf5c83f7c03b8246
Sonali Prakash Patil <106173021+sonappatil@users.noreply.github.com>
2024-11-30 00:30:30 +0530

    Update adminController.js

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -52,3 +52,45 @@ exports.modifyUserDetails = asyncHandler(async (req, res) => {
       res.status(500).send({ message: error.message });
     }
   });
+
+  exports.modifyAdminDetails = asyncHandler(async (req, res) => {
+    try {
+      const updatedAdmin = await Admin.findById(req.params.id);
+         // console.log(updatedUser);
+      if (!updatedAdmin) {
+        return res.status(404).send({ message: "Admin not found" });
+      }
+       
+      const { email, phone, name, password, role} = req.body;
+    const profileImage = req.file ? req.file.filename : updatedAdmin.profileImage;
+
+      const updateAdmin = await Admin.findByIdAndUpdate(
+        req.params.id,
+        {email, phone, name, password, role,profileImage},
+        { new: true }
+      );
+  
+      res.status(200).send(updateAdmin);
+     // console.log(updateUser);
+    } catch (error) {
+      console.log(error);
+      res.status(400).send({ message: error.message });
+    }
+  });
+
+  exports.deleteAdmin = asyncHandler(async (req, res) => {
+    try {
+      const deletedAdmin = await Admin.findById(req.params.id);
+  
+      if (!deletedAdmin) {
+        return res.status(404).send({ message: "Admin not found" });
+      }
+  
+      await Admin.findByIdAndDelete(req.params.id);
+  
+      res.status(200).send({ message: "Admin is deleted successfully" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ message: error.message });
+    }
+  });
