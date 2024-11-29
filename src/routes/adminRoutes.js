5c28692b1082ceb981d2989d5b46526a94e89c68
Sonali Prakash Patil <106173021+sonappatil@users.noreply.github.com>
2024-11-30 00:31:12 +0530

    Update adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -3,15 +3,16 @@ const adminController = require("../controllers/adminController");
 const authenticationMiddleware = require("../middlewares/authentication");
 const authorizationMiddleware = require("../middlewares/authorization");
 const userUpload = require("../multer/userMulter");
+const adminUpload = require("../multer/adminMulter");
 
 const adminRouter = express.Router();
 
 adminRouter.use(authenticationMiddleware);
 
 adminRouter.get("/admin/users" , authorizationMiddleware("Admin"), adminController.getAllUsers);
-adminRouter.put("/admin/:id" , authorizationMiddleware("Admin"), userUpload.single("profileImage"), adminController.modifyUserDetails);
-adminRouter.delete("/admin/:id", authorizationMiddleware("Admin"), adminController.deleteUser);
-
-
+adminRouter.put("/admin/user/:id" , authorizationMiddleware("Admin"), userUpload.single("profileImage"), adminController.modifyUserDetails);
+adminRouter.delete("/admin/user/:id", authorizationMiddleware("Admin"), adminController.deleteUser);
+adminRouter.put("/admin/:id" , authorizationMiddleware("Admin"), adminUpload.single("profileImage"), adminController.modifyAdminDetails);
+adminRouter.delete("/admin/:id", authorizationMiddleware("Admin"), adminController.deleteAdmin);
 
 module.exports = adminRouter;
